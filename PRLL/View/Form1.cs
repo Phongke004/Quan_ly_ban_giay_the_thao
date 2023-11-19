d918395419d1ab6a6372cae28bd968049ae94cec
KieuPhucs <maumebebet54@gmail.com>
2023-11-20 01:18:26 +0700

    LAMtay

--- PRLL/View/Form1.cs
+++ PRLL/View/Form1.cs
@@ -16,5 +16,22 @@ namespace PRL.View
         {
             InitializeComponent();
         }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
